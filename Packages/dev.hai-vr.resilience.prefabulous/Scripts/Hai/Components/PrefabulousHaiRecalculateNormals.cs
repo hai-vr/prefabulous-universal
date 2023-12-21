654d1cb4bcfd85c9c4355a6b8ba4b92d666d1ec6
Haï~ <hai-vr@users.noreply.github.com>
2023-12-22 00:52:40 +0100

    Add Recalculate Normals: - Recalculate Normals will recalculate select blendshape normals of SkinnedMeshRenderers (and tangents). - Blendshape delta normals (and tangents) are usually zero on avatar uploads.   - This may cause the avatar shading to look abnormal, especially on blendshapes that flatten part of the body, or significantly push it out. - This component attempts to recalculate normals of individual blendshapes by doing the following:   - Bake a static mesh of a SMR mesh when 0 blendshapes are running   - Build a data structure describing vertices that have same position and normal   - Use RecalculateNormals. This is done even when 0 blendshapes are active, so that the delta is calculated based on Unity's judgement of what's the normal data at rest.   - Using that previous data structure, rebuild normal data by averaging and renormalizing vertex normals that had the same position and normal     - This is done to fix an issue where an artist-authored vertex is part of an UV seam, causing the mesh data to split that vertex into multiple, effectively causing the RecalculateNormals output to have different normals for that vertex.   - Use RecalculateTangents.   - Do all of the above again for each blendshape to recalculate, and use that to calculate a delta normal and a delta tangent.   - Once all blendshapes are processed, effectively rewrite all of the mesh blendshapes with these modifications.

---
+++ Packages/dev.hai-vr.resilience.prefabulous/Scripts/Hai/Components/PrefabulousHaiRecalculateNormals.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using VRC.SDKBase;
+
+namespace Prefabulous.Hai.Runtime
+{
+    [AddComponentMenu("Prefabulous Avatar/PA-H Recalculate Normals")]
+    public class PrefabulousHaiRecalculateNormals : MonoBehaviour, IEditorOnly
+    {
+        public string[] blendShapes;
+        public bool limitToSpecificMeshes;
+        public SkinnedMeshRenderer[] renderers;
+    }
+}
---
+++ Packages/dev.hai-vr.resilience.prefabulous/Scripts/VRC/Editor/PrefabulousHaiRecalculateNormalsPlugin.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using nadena.dev.ndmf;
+using Prefabulous.Hai.Runtime;
+using Prefabulous.VRC.Editor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+[assembly: ExportsPlugin(typeof(PrefabulousHaiRecalculateNormalsPlugin))]
+namespace Prefabulous.VRC.Editor
+{
+    public class PrefabulousHaiRecalculateNormalsPlugin : Plugin<PrefabulousHaiRecalculateNormalsPlugin>
+    {
+        protected override void Configure()
+        {
+            var seq = InPhase(BuildPhase.Transforming)
+                .BeforePlugin("Hai.FaceTraShape.Editor.HFTSCPlugin");
+            
+            seq.Run("Recalculate Normals", RecalculateNormals);
+        }
+
+        private void RecalculateNormals(BuildContext context)
+        {
+            var recalculates = context.AvatarRootTransform.GetComponentsInChildren<PrefabulousHaiRecalculateNormals>(true);
+            if (recalculates.Length == 0) return;
+
+            var smrs = context.AvatarRootTransform.GetComponentsInChildren<SkinnedMeshRenderer>(true);
+            foreach (var smr in smrs)
+            {
+                var mesh = smr.sharedMesh;
+                if (mesh == null) continue;
+                
+                var thatSmrBlendShapes = Enumerable.Range(0, mesh.blendShapeCount)
+                    .Select(i => mesh.GetBlendShapeName(i))
+                    .ToList();
+                var applicableBlendShapes = recalculates
+                    .Where(recalculate => !recalculate.limitToSpecificMeshes || recalculate.renderers.Contains(smr))
+                    .SelectMany(normals => normals.blendShapes)
+                    .Where(blendShape => thatSmrBlendShapes.Contains(blendShape))
+                    .ToList();
+                if (applicableBlendShapes.Count > 0)
+                {
+                    RecalculateNormalsOf(smr, thatSmrBlendShapes, applicableBlendShapes);
+                }
+            }
+        }
+
+        private void RecalculateNormalsOf(SkinnedMeshRenderer smr, List<string> thatSmrBlendShapes, List<string> applicableBlendShapes)
+        {
+            // TODO: If multiple SMRs share the same sharedMesh, it may not be necessary to do this op on all of them
+            // However, it's rare for a single avatar to be referencing the same SMR mesh mutliple times.
+            var originalMesh = smr.sharedMesh;
+            
+            var baker = Object.Instantiate(smr);
+            for (var i = 0; i < thatSmrBlendShapes.Count; i++)
+            {
+                baker.SetBlendShapeWeight(i, 0);
+            }
+
+            var bake0mesh = new Mesh();
+            baker.BakeMesh(bake0mesh);
+            
+            // RecalculateNormals fails to calculate normals aesthetically when an artist-authored vertex is part of an UV seam,
+            // which turns it into multiple vertices in the engine mesh data representation.
+            // To fix this, we store the indices that have the same position and normal (pre-recalculate normals),
+            // and then reuse these indices to re-recalculate an "average normal", which isn't always accurate but is a good trade-off.
+            var indicesWithSamePosNorm = StoreIndicesWithSamePositionAndNormal(bake0mesh);
+            // We're recalculating normals on a 0-mesh because we want the delta to be based on the diff with Unity's RecalculateNormals judgement
+            bake0mesh.RecalculateNormals();
+            ReRecalculateNormalsInUVSeams(bake0mesh, indicesWithSamePosNorm);
+            bake0mesh.RecalculateTangents();
+            
+            // Export normals and tangents now to avoid Unity extern access
+            var bake0MeshNormals = bake0mesh.normals;
+            var bake0MeshTangents = bake0mesh.tangents;
+
+            var nameToFrameDeltaBakes = new Dictionary<string, DeltaMeshBake[]>();
+
+            var _ignored = new Vector3[originalMesh.vertexCount];
+            var deltaVertices = new Vector3[originalMesh.vertexCount];
+            var deltaNormals = new Vector3[originalMesh.vertexCount];
+            var deltaTangents = new Vector3[originalMesh.vertexCount];
+
+            foreach (var applicableBlendShape in applicableBlendShapes)
+            {
+                var shapeIndex = thatSmrBlendShapes.IndexOf(applicableBlendShape);
+                var frameCount = originalMesh.GetBlendShapeFrameCount(shapeIndex);
+
+                var meshBake = new DeltaMeshBake[frameCount];
+                nameToFrameDeltaBakes[applicableBlendShape] = meshBake;
+                
+                for (var frameIndex = 0; frameIndex < frameCount; frameIndex++)
+                {
+                    var frameWeight = originalMesh.GetBlendShapeFrameWeight(shapeIndex, frameIndex);
+
+                    var bakedShape = new Mesh();
+                
+                    baker.SetBlendShapeWeight(shapeIndex, frameWeight);
+                    baker.BakeMesh(bakedShape);
+                    baker.SetBlendShapeWeight(shapeIndex, 0);
+                
+                    // We need to recalculate the indices because it's possible that a blendshape splits the vertices apart,
+                    // even if they had the same PosNorm (i.e. something that opens clothing in half)
+                    var indicesInBakedWithSamePosNorm = StoreIndicesWithSamePositionAndNormal(bakedShape);
+                    bakedShape.RecalculateNormals();
+                    ReRecalculateNormalsInUVSeams(bakedShape, indicesInBakedWithSamePosNorm);
+                    bakedShape.RecalculateTangents();
+
+                    // Export normals and tangents now to avoid Unity extern access
+                    var bakedShapeNormals = bakedShape.normals;
+                    var bakedShapeTangents = bakedShape.tangents;
+
+                    originalMesh.GetBlendShapeFrameVertices(shapeIndex, frameIndex, deltaVertices, _ignored, _ignored);
+                    for (var i = 0; i < originalMesh.vertexCount; i++)
+                    {
+                        // Zero-valued vertices still need normals and tangents updated based on neighbouring non-zero-valued vertices.
+                        // This is especially visible on blendshapes that "flatten" something, where zero-valued vertices still need delta normals applied to avoid an "aura" bump.
+                        // Therefore execute this even when the delta vertex is equal to zero.
+                        deltaNormals[i] = bakedShapeNormals[i] - bake0MeshNormals[i];
+                        deltaTangents[i] = (Vector3)(bakedShapeTangents[i] - bake0MeshTangents[i]); // TODO: What's the deal with the binormal (Vector4 tangents)?
+                    }
+                    
+                    nameToFrameDeltaBakes[applicableBlendShape][frameIndex].vertices = deltaVertices.ToArray();
+                    nameToFrameDeltaBakes[applicableBlendShape][frameIndex].normals = deltaNormals.ToArray();
+                    nameToFrameDeltaBakes[applicableBlendShape][frameIndex].tangents = deltaTangents.ToArray();
+                }
+            }
+            
+            Object.DestroyImmediate(baker.gameObject);
+            
+            var mesh = Object.Instantiate(originalMesh);
+            RebuildBlendshapesOnCopy(mesh, originalMesh, nameToFrameDeltaBakes);
+            smr.sharedMesh = mesh;
+        }
+
+        private void ReRecalculateNormalsInUVSeams(Mesh mesh, List<int[]> indicesWithSamePosNorm)
+        {
+            var normals = mesh.normals;
+            foreach (var indices in indicesWithSamePosNorm)
+            {
+                var normal = Vector3.zero;
+                foreach (var i in indices)
+                {
+                    normal += normals[i];
+                }
+                normal /= indices.Length;
+                normal = normal.normalized;
+                
+                foreach (var i in indices)
+                {
+                    normals[i] = normal;
+                }
+            }
+
+            mesh.normals = normals;
+        }
+
+        private List<int[]> StoreIndicesWithSamePositionAndNormal(Mesh bake0Mesh)
+        {
+            // We do not want to include the tangent, because the tangent is determined by the UV seam.
+            // (Correcting for UV seams is the main reason we're doing this)
+            
+            var vertices = bake0Mesh.vertices;
+            var normals = bake0Mesh.normals;
+
+            var posNormToIndex = new Dictionary<PosNorm, List<int>>();
+            for (var index = 0; index < vertices.Length; index++)
+            {
+                var posNorm = new PosNorm(vertices[index], normals[index]);
+                if (!posNormToIndex.ContainsKey(posNorm))
+                {
+                    posNormToIndex.Add(posNorm, new List<int>());
+                }
+                posNormToIndex[posNorm].Add(index);
+            }
+
+            return posNormToIndex
+                .Where(pair => pair.Value.Count >= 2)
+                .Select(pair => pair.Value.ToArray())
+                .ToList();
+        }
+
+        public struct PosNorm
+        {
+            public PosNorm(Vector3 position, Vector3 normal)
+            {
+                this.position = position;
+                this.normal = normal;
+            }
+
+            public Vector3 position;
+            public Vector3 normal;
+
+            public bool Equals(PosNorm other)
+            {
+                return position.Equals(other.position) && normal.Equals(other.normal);
+            }
+
+            public override bool Equals(object obj)
+            {
+                return obj is PosNorm other && Equals(other);
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(position, normal);
+            }
+        }
+
+        private static void RebuildBlendshapesOnCopy(Mesh meshCopy, Mesh originalMesh, Dictionary<string, DeltaMeshBake[]> nameToFrameDeltaBakes)
+        {
+            var verts = new Vector3[originalMesh.vertexCount];
+            var norms = new Vector3[originalMesh.vertexCount];
+            var tans = new Vector3[originalMesh.vertexCount];
+
+            meshCopy.ClearBlendShapes();
+            for (var shapeIndex = 0; shapeIndex < originalMesh.blendShapeCount; shapeIndex++)
+            {
+                var name = originalMesh.GetBlendShapeName(shapeIndex);
+                if (!nameToFrameDeltaBakes.Keys.Contains(name))
+                {
+                    var frames = originalMesh.GetBlendShapeFrameCount(shapeIndex);
+                    for (var frameIndex = 0; frameIndex < frames; frameIndex++)
+                    {
+                        var weight = originalMesh.GetBlendShapeFrameWeight(shapeIndex, frameIndex);
+                        originalMesh.GetBlendShapeFrameVertices(shapeIndex, frameIndex, verts, norms, tans);
+                        meshCopy.AddBlendShapeFrame(name, weight, verts, norms, tans);
+                    }
+                }
+                else
+                {
+                    var baked = nameToFrameDeltaBakes[name];
+                    var frames = baked.Length;
+                    for (var frameIndex = 0; frameIndex < frames; frameIndex++)
+                    {
+                        var weight = originalMesh.GetBlendShapeFrameWeight(shapeIndex, frameIndex);
+                        var bakedFrame = baked[frameIndex];
+                        meshCopy.AddBlendShapeFrame(name, weight, bakedFrame.vertices, bakedFrame.normals, bakedFrame.tangents);
+                    }
+                }
+            }
+        }
+    }
+
+    internal class VxDataStruct
+    {
+    }
+
+    internal struct DeltaMeshBake
+    {
+        public Vector3[] vertices;
+        public Vector3[] normals;
+        public Vector3[] tangents;
+    }
+}
