f5714177e330a82d5d05ba8044458978d215823b
Haï~ <hai-vr@users.noreply.github.com>
2024-03-27 12:42:59 +0100

    Fix PrefabulousHaiMassBlendshapeSync was missing IEditorOnly tag: - Add missing IEditorOnly to PrefabulousHaiMassBlendshapeSync.

--- Packages/dev.hai-vr.resilience.prefabulous/Scripts/Hai/Components/PrefabulousHaiMassBlendshapeSync.cs
+++ Packages/dev.hai-vr.resilience.prefabulous/Scripts/Hai/Components/PrefabulousHaiMassBlendshapeSync.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
+using VRC.SDKBase;
 
 namespace Prefabulous.Hai.Runtime
 {
     [AddComponentMenu("Prefabulous Avatar/PA-H Mass Blendshape Sync")]
-    public class PrefabulousHaiMassBlendshapeSync : MonoBehaviour
+    public class PrefabulousHaiMassBlendshapeSync : MonoBehaviour, IEditorOnly
     {
         public SkinnedMeshRenderer source;
         public SkinnedMeshRenderer[] targets;
