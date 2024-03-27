1c608a5e841c229e45ee11a5d096dea01ab6ecc4
Haï~ <hai-vr@users.noreply.github.com>
2024-03-27 03:12:03 +0100

    Fix Unity 2019 compatibility in ConvertBlendshapeConventions: - PrefabulousHaiConvertBlendshapeConventions.cs(27,49): error CS1503: Argument 1: cannot convert from 'string' to 'char'

--- Packages/dev.hai-vr.resilience.prefabulous/Scripts/Hai/Components/PrefabulousHaiConvertBlendshapeConventions.cs
+++ Packages/dev.hai-vr.resilience.prefabulous/Scripts/Hai/Components/PrefabulousHaiConvertBlendshapeConventions.cs
@@ -18,13 +18,19 @@ namespace Prefabulous.Hai.Runtime
 
         public Dictionary<string, string> ParseMapping()
         {
+            var mapping = new Dictionary<string, string>();
+            if (keyValueSeparator.Length != 1)
+            {
+                return mapping;
+            }
+            char separator = keyValueSeparator.ToCharArray()[0]; // Unity 2019 compat.
+            
             var store = keyValueMapping ?? "";
 
-            var mapping = new Dictionary<string, string>();
             var lines = store.Split('\n').Where(s => !string.IsNullOrWhiteSpace(s));
             foreach (var keyValue in lines)
             {
-                var equalSplit = keyValue.Split(keyValueSeparator);
+                var equalSplit = keyValue.Split(separator);
                 if (equalSplit.Length == 2)
                 {
                     var key = equalSplit[reverse ? 1 : 0];
