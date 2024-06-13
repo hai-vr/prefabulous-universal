fdc1d848acf3347c38f89521e550a728c23ef687
Haï~ <hai-vr@users.noreply.github.com>
2024-06-13 08:28:02 +0200

    Fix compilation error in non-VRC projects

--- Packages/dev.hai-vr.resilience.prefabulous.universal/Scripts/Common/PrefabulousGenerateTwistBones.cs
+++ Packages/dev.hai-vr.resilience.prefabulous.universal/Scripts/Common/PrefabulousGenerateTwistBones.cs
@@ -1,5 +1,9 @@
 ﻿using UnityEngine;
+#if VRC_SDK_VRCSDK3
 using IPrefabulousEditorOnly = VRC.SDKBase.IEditorOnly;
+#else
+using Prefabulous.Universal.Shared.Runtime;
+#endif
 
 namespace Prefabulous.Universal.Common.Runtime
 {
