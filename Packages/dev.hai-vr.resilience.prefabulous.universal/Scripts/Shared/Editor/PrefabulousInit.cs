c4ccbffa5fb115e82d691c21feaac0203abccfed
Haï~ <hai-vr@users.noreply.github.com>
2024-06-28 14:29:00 +0200

    Hide all Prefabulous icons from the scene: - Hide all Prefabulous icons from the scene in Unity 2022 or above.

---
+++ Packages/dev.hai-vr.resilience.prefabulous.universal/Scripts/Shared/Editor/PrefabulousInit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Prefabulous.Universal.Shared.Editor
+{
+    [InitializeOnLoad]
+    public class PrefabulousInit
+    {
+        static PrefabulousInit()
+        {
+            EditorApplication.delayCall += Next;
+        }
+
+        private static void Next()
+        {
+            // GizmoUtility.SetIconEnabled does not appear to exist in Unity 2021
+#if UNITY_2022_1_OR_NEWER
+            var allPrefabulousTypes = FindAllPrefabulousMonoTypes();
+            foreach (var prefabulousType in allPrefabulousTypes)
+            {
+                GizmoUtility.SetIconEnabled(prefabulousType, false);
+            }
+#endif
+        }
+
+        private static Type[] FindAllPrefabulousMonoTypes()
+        {
+            var allPrefabulousTypes = AppDomain.CurrentDomain
+                .GetAssemblies()
+                .SelectMany(assembly => assembly.GetTypes())
+                .Where(type => typeof(MonoBehaviour).IsAssignableFrom(type))
+                .Where(type => type.Name.StartsWith("Prefabulous"))
+                .ToArray();
+            return allPrefabulousTypes;
+        }
+    }
+}
