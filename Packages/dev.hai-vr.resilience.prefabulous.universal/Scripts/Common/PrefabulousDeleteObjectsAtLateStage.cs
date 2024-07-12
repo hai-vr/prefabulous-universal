476c18e4d1662c8cd22a37586f429ff921fbd957
Haï~ <hai-vr@users.noreply.github.com>
2024-07-12 21:55:55 +0200

    Add internal "Delete Objects At Late Stage": - Add "Delete Objects At Late Stage" for internal use:   - This component allows the deletion of objects at a late stage so that their reference in other components is preserved at an earlier stage for testing purposes.

---
+++ Packages/dev.hai-vr.resilience.prefabulous.universal/Scripts/Common/PrefabulousDeleteObjectsAtLateStage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+#if VRC_SDK_VRCSDK3
+using IPrefabulousEditorOnly = VRC.SDKBase.IEditorOnly;
+#else
+using Prefabulous.Universal.Shared.Runtime;
+#endif
+
+namespace Prefabulous.Universal.Common.Runtime
+{
+#if PREFABULOUS_INTERNAL
+    [AddComponentMenu("Prefabulous/PA Delete Objects At Late Stage")]
+#else
+    [AddComponentMenu("")]
+#endif
+    public class PrefabulousDeleteObjectsAtLateStage : MonoBehaviour, IPrefabulousEditorOnly
+    {
+        public GameObject[] objects = Array.Empty<GameObject>();
+        public bool deleteThisObject = true;
+    }
+}
---
+++ Packages/dev.hai-vr.resilience.prefabulous.universal/Scripts/Shared/Editor/PrefabulousDeleteObjectsAtLateStagePlugin.cs
@@ -0,0 +1,57 @@
+﻿#if PREFABULOUS_INTERNAL
+using System.Linq;
+using nadena.dev.ndmf;
+using Prefabulous.Universal.Common.Runtime;
+using Prefabulous.Universal.Shared.Editor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+[assembly: ExportsPlugin(typeof(PrefabulousDeleteObjectsAtLateStagePlugin))]
+namespace Prefabulous.Universal.Shared.Editor
+{
+    public class PrefabulousDeleteObjectsAtLateStagePlugin : Plugin<PrefabulousDeleteObjectsAtLateStagePlugin>
+    {
+        public override string QualifiedName => "dev.hai-vr.prefabulous.universal.DeleteObjectsAtLateStage";
+        public override string DisplayName => "Prefabulous Universal - Delete Objects At Late Stage";
+
+        protected override void Configure()
+        {
+            var seq = InPhase(BuildPhase.Optimizing);
+
+            seq
+                .AfterPlugin("com.anatawa12.avatar-optimizer")
+                .Run("Delete Objects At Late Stage", DeleteObjects);
+        }
+
+        private void DeleteObjects(BuildContext context)
+        {
+            var delete = context.AvatarRootTransform.GetComponentsInChildren<PrefabulousDeleteObjectsAtLateStage>(true);
+            if (delete.Length == 0) return;
+
+            var objectsToDelete = delete
+                .SelectMany(comp =>
+                {
+                    var objs = comp.objects != null ? comp.objects.Where(o => o != null) : Enumerable.Empty<GameObject>();
+                    if (comp.deleteThisObject)
+                    {
+                        return objs.Concat(new[] { comp.gameObject });
+                    }
+
+                    return objs;
+                })
+                .Distinct()
+                .ToArray();
+            
+            foreach (var gameObject in objectsToDelete)
+            {
+                if (gameObject != null) // Could happen in hierarchy deletions
+                {
+                    Object.DestroyImmediate(gameObject);
+                }
+            }
+
+            PrefabulousUtil.DestroyAllAfterBake<PrefabulousDeleteObjectsAtLateStage>(context);
+        }
+    }
+}
+#endif
